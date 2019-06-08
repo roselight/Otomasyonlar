902743c6d32f01e71066a53d3c0d5af73a00f758
Gülnur <nur_412@windowslive.com>
2019-06-08 11:35:27 +0300

    Factory Tasarım Deseni

---
+++ Factory Pattern/factory/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace factory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fiyatını Öğrenmek İstediğiniz Kumaş Numarasını Giriniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "İPEK İÇİN (0)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "KADİFE İÇİN (1)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "SATEN İÇİN (2)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "PAMUKLU İÇİN (3)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "KOTON İÇİN (4)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(395, 28);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(224, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GETİR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 243);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FACTORY METHOD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Factory Pattern/factory/Kumas.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory
+{
+     public interface Kumas
+    {
+        double getkumasfiyat();
+        string getkumasadi();
+
+    }
+    public class Ipek : Kumas
+    {
+        private double _fiyat;
+        public Ipek(double fiyat)
+        {
+            _fiyat = fiyat;
+        }
+        public string getkumasadi()
+        {
+            return "İpek Kumaş";
+        }
+        public double getkumasfiyat()
+        {
+            return _fiyat;
+        }
+    }
+    public class Kadife:Kumas
+    {
+        private double _fiyat;
+        public Kadife(double fiyat)
+        {
+            _fiyat = fiyat;
+        }
+        public string getkumasadi()
+        {
+            return "Kadife Kumaş";
+        }
+        public double getkumasfiyat()
+        {
+            return _fiyat;
+        }
+    }
+    public class Saten:Kumas
+        {
+        private double _fiyat;
+        public Saten(double fiyat)
+        {
+            _fiyat = fiyat;
+        }
+        public string getkumasadi()
+        {
+            return "Saten Kumaş";
+        }
+        public double getkumasfiyat()
+        {
+            return _fiyat;
+        }
+    }
+        public class Pamuklu:Kumas
+            {
+            private double _fiyat;
+            public Pamuklu(double fiyat)
+        {
+            _fiyat = fiyat;
+        }
+        public string getkumasadi()
+        {
+            return "Pamuklu Kumaş";
+        }
+        public double getkumasfiyat()
+        {
+            return _fiyat;
+        }
+    }
+           
+    public class Koton :Kumas
+    {
+        private double _fiyat;
+        public Koton(double fiyat)
+        {
+            _fiyat = fiyat;
+        }
+        public string getkumasadi()
+        {
+            return "Koton Kumaş";
+        }
+        public double getkumasfiyat()
+        {
+            return _fiyat;
+        }
+    }
+    static class Factory
+    {
+        public static Kumas Get(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return new Ipek(30.90);
+                case 1:
+                    return new Kadife(15.50);
+                case 2:
+                    return new Saten(19.40);
+                case 3:
+                    return new Pamuklu(7.85);
+                case 4:
+                    return new Koton(9.35);
+                default:
+                    return null;
+
+            }
+        }
+    }
+}
+
